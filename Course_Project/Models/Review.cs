b632c46d5c4f066ddf7803f919b56f1adea65505
Ruslan Skytsko <Gaveloshpom@gmail.com>
2025-05-19 16:21:08 +0200

    UnitTests

--- Course_Project/Models/Review.cs
+++ Course_Project/Models/Review.cs
@@ -1,22 +1,22 @@
-﻿using System;
-using Newtonsoft.Json;
+﻿//using System;
+//using Newtonsoft.Json;
 
-namespace Course_Project.Models
-{
-    [JsonObject]
-    public class Review
-    {
-        [JsonProperty]
-        public int Rating { get; set; } // від 1 до 5
+//namespace Course_Project.Models
+//{
+//    [JsonObject]
+//    public class Review
+//    {
+//        [JsonProperty]
+//        public int Rating { get; set; } // від 1 до 5
 
-        [JsonProperty]
-        public string Text { get; set; }
+//        [JsonProperty]
+//        public string Text { get; set; }
 
-        [JsonProperty]
-        public RegisteredUser Author { get; set; }
+//        [JsonProperty]
+//        public RegisteredUser Author { get; set; }
 
-        [JsonProperty]
-        public DateTime Date { get; set; }
-    }
-}
+//        [JsonProperty]
+//        public DateTime Date { get; set; }
+//    }
+//}
 
---
+++ UnitTest/ContentBlockTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Course_Project.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class ContentBlockTests
+    {
+        [TestMethod]
+        public void Title_ReturnsLectureTitle()
+        {
+            var block = new ContentBlock
+            {
+                Type = "Лекція",
+                LectureData = new Lecture { Title = "Тестова лекція" }
+            };
+
+            Assert.AreEqual("Тестова лекція", block.Title);
+        }
+
+        [TestMethod]
+        public void Title_ReturnsPracticeTitle()
+        {
+            var block = new ContentBlock
+            {
+                Type = "Практика",
+                PracticeData = new Practice { Title = "Тестова практика" }
+            };
+
+            Assert.AreEqual("Тестова практика", block.Title);
+        }
+    }
+}
